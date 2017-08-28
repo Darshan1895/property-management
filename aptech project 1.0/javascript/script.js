c713378996e6b79bb4a6cec7cd100bfa33871bdc
Darshan1895 <31367278+Darshan1895@users.noreply.github.com>
2017-08-28 13:08:33 +0530

    Add files via upload

---
+++ aptech project 1.0/javascript/script.js
@@ -0,0 +1,40 @@
+//log and sign in pop up
+
+
+function opensign(){
+     var a= document.getElementById("box");
+     a.style.display="block";
+   }
+   function closesign(){
+     var b= document.getElementById("box");
+     b.style.display="none";
+   }
+function openlog(){
+     var a= document.getElementById("box1");
+     a.style.display="block";
+   }
+   function closelog(){
+     var b= document.getElementById("box1");
+     b.style.display="none";
+   }
+
+function openlog_in(){
+var b=document.getElementById("box1");
+var c=document.getElementById("box");
+b.style.display="block";
+c.style.display="none";
+}
+function opensign_in(){
+var b=document.getElementById("box1");
+var c=document.getElementById("box");
+b.style.display="none";
+c.style.display="block";
+}
+
+
+function closebox(){
+var b=document.getElementById("box");
+var c=document.getElementById("box1");
+b.style.display="none";
+c.style.display="none";
+}
